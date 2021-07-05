2d82bb08b1f9f5366e59545d5cdc466465ad9f23
shizi <2054701360@qq.com>
2021-07-05 11:53:32 +0800

    shizi

--- src/main/java/yinyou.java
+++ src/main/java/yinyou.java
@@ -1,5 +1,7 @@
 public class yinyou {
     public static void main(String[] args) {
-        System.out.println("yinyou");
+        System.out.println("yinyou"); 
+        System.out.println("shizi");
+        System.out.println("xuhongyu");
     }
 }
